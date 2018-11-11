05cac2a6dd8dbab650e01ef4cfb201e83544388c
jiro4989 <jiroron666@gmail.com>
2018-11-11 23:05:20 +0900

    [add]C++のコードを追加

---
+++ src/cpp/01_cmdargs/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cout << "need 2 arguments." << endl;
+    return 1;
+  }
+
+  auto x = atoi(argv[1]);
+  auto y = atoi(argv[2]);
+  auto sum = x + y;
+  cout << "sum:" << sum << endl;
+
+  return 0;
+}
